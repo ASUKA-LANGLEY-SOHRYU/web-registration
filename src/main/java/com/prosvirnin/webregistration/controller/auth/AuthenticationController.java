f41cea6d1d734b52938f6cbfcd30560d4f9ae019
ASUKA-LANGLEY-SOHRYU <mishaprosvirninmail@yandex.ru>
2024-04-26 12:44:19 +0500

    Added exception handler

--- src/main/java/com/prosvirnin/webregistration/controller/auth/AuthenticationController.java
+++ src/main/java/com/prosvirnin/webregistration/controller/auth/AuthenticationController.java
@@ -1,9 +1,11 @@
 package com.prosvirnin.webregistration.controller.auth;
 
+import com.prosvirnin.webregistration.exception.auth.EmailAlreadyExistsException;
 import com.prosvirnin.webregistration.model.auth.AuthenticationRequest;
 import com.prosvirnin.webregistration.model.auth.AuthenticationResponse;
 import com.prosvirnin.webregistration.service.auth.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,5 +33,13 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request)
     {
         return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<AuthenticationResponse> handleException(EmailAlreadyExistsException e){
+        return ResponseEntity.ok(AuthenticationResponse.builder()
+                .error(e.getMessage())
+                .build());
     }
 }
--- src/main/java/com/prosvirnin/webregistration/model/auth/AuthenticationRequest.java
+++ src/main/java/com/prosvirnin/webregistration/model/auth/AuthenticationRequest.java
@@ -1,8 +1,10 @@
 package com.prosvirnin.webregistration.model.auth;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AuthenticationRequest {
     String email;
     String password;
